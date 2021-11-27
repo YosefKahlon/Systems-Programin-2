13ba67d05c9727fbd6e33c6f2dcd983057f5072d
yosefkahlon <yosefkahlon53@gmail.com>
2021-11-27 11:44:07 +0200

    mat

--- my_mat.c
+++ my_mat.c
@@ -1,69 +1,59 @@
 #include <stdio.h>
+#include <stdio.h>
+#include "my_mat.h"
 
-//set  (i,j)
-void fucn1(int mat[10][10])
+int main()
 {
-    int user;
-
-    for (int i = 0; i < 10; i++)
+   int mat[10][10];
+   
+    char user;
+    scanf("%c", &user);
+    while (user != 'D')
     {
-        for (int j = 0; j < 10; j++)
+        if (user == 'A')
         {
-            scanf("%d", &user);
-            mat[i][j] = user;
+            func1(mat);
+            
         }
+        else if (user == 'B')
+        {
+            int ans=func2(mat);
+            if (ans == 0)
+        {
+            printf("False");
+            printf("\n");
+        }
+    else
+        {
+            printf("True");
+            printf("\n");
+        }
+            
+        }
+        else if (user == 'C')
+        {
+           int ans= func3(mat);
+           if (ans == 0)
+    {
+        printf("-1");
+        printf("\n");
     }
+    else
+    {
+        printf("%d", ans);
+        printf("\n");
+    }
+         }
+        if (user=='D'){
+            return 0;
+        }else{
+            scanf("%c", &user); 
+        }
+ 
+        
+    }
+   return 0;
 }
-
-//_______q2_____________________________
-
-int func2(int mat[3][3])
-{
-    int row, col;
-    int ten = 10 ;
-
-    scanf("d", &row);
-    scanf("d", &col);
-
-    int dist[ten][ten], i, j, k;
-
-    for (i = 0; i < ten; i++)
-    {
-        for (j = 0; j < ten; j++)
-        {
-
-            dist[i][j] = mat[i][j];
-        }
-    }
-
-    for (k = 0; k < ten; k++)
-    {
-        // Pick all vertices as source one by one
-        for (i = 0; i < ten; i++)
-        {
-            // Pick all vertices as destination for the
-            // above picked source
-            for (j = 0; j < ten; j++)
-            {
-
-                if (dist[i][k] == 0 || dist[k][j] == 0)
-                {
-                    continue;
-                }
-                if (dist[i][j] == 0 && i != j)
-                {
-                    dist[i][j] == dist[i][k] + dist[k][j];
-                }
-                else if (dist[i][k] + dist[k][j] < dist[i][j])
-                {
-                    dist[i][j] =dist[i][k] + dist[k][j];
-                }
-            }
-        }
-    }
-    return dist[row][col];
-}
-
 
 
   
