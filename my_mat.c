5ef1b6abb35d45e37ca6603e1f2dd08d774140ca
yosefkahlon <yosefkahlon53@gmail.com>
2021-11-27 11:50:08 +0200

    mat.c

--- my_mat.c
+++ my_mat.c
@@ -1,59 +1,120 @@
 #include <stdio.h>
-#include <stdio.h>
 #include "my_mat.h"
 
-int main()
+void func1(int mat[10][10])
 {
-   int mat[10][10];
-   
-    char user;
-    scanf("%c", &user);
-    while (user != 'D')
+    int user;
+
+  //  for (int i = 0; i < 10; i++)
+   // {
+      //  for (int b = 0; b < 10; b++)
+     //  {
+        //    mat[i][b] = 0;
+       // }
+   // }
+
+    for (int i = 0; i < 10; i++)
     {
-        if (user == 'A')
+        for (int b = 0; b < 10; b++)
         {
-            func1(mat);
-            
+            scanf("%d", &user);
+            mat[i][b] = user;
         }
-        else if (user == 'B')
-        {
-            int ans=func2(mat);
-            if (ans == 0)
-        {
-            printf("False");
-            printf("\n");
-        }
-    else
-        {
-            printf("True");
-            printf("\n");
-        }
-            
-        }
-        else if (user == 'C')
-        {
-           int ans= func3(mat);
-           if (ans == 0)
-    {
-        printf("-1");
-        printf("\n");
+
+        /* code */
     }
-    else
-    {
-        printf("%d", ans);
-        printf("\n");
-    }
-         }
-        if (user=='D'){
-            return 0;
-        }else{
-            scanf("%c", &user); 
-        }
- 
-        
-    }
-   return 0;
 }
 
+int func2(int mat[10][10])
+{
+    int len;
+    int row;
+    scanf("%d", &len);
+    scanf("%d", &row);
+
+    int dist[10][10], i, j, k;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 10; j++)
+        {
+            dist[i][j] = mat[i][j];
+        }
+    }
+
+    for (k = 0; k < 10; k++)
+    {
+        // Pick all vertices as source one by one
+        for (i = 0; i < 10; i++)
+        {
+            // Pick all vertices as destination for the
+            // above picked source
+            for (j = 0; j < 10; j++)
+            {
+                if (dist[i][k] == 0 || dist[k][j] == 0)
+                {
+                    continue;
+                }
+                if (dist[i][j] == 0 && i != j)
+                {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+                // If vertex k is on the shortest path from
+                // i to j, then update the value of dist[i][j]
+                else if (dist[i][k] + dist[k][j] < dist[i][j])
+                {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+    return dist[len][row];
+}
+
+int func3(int mat[10][10])
+{
+    int len;
+    int row;
+    scanf("%d", &len);
+    scanf("%d", &row);
+
+    int dist[10][10], i, j, k;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 10; j++)
+        {
+            dist[i][j] = mat[i][j];
+        }
+    }
+
+    for (k = 0; k < 10; k++)
+    {
+        // Pick all vertices as source one by one
+        for (i = 0; i < 10; i++)
+        {
+            // Pick all vertices as destination for the
+            // above picked source
+            for (j = 0; j < 10; j++)
+            {
+                if (dist[i][k] == 0 || dist[k][j] == 0)
+                {
+                    continue;
+                }
+                if (dist[i][j] == 0 && i != j)
+                {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+                // If vertex k is on the shortest path from
+                // i to j, then update the value of dist[i][j]
+                else if (dist[i][k] + dist[k][j] < dist[i][j])
+                {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+    return dist[len][row];
+}
 
   
