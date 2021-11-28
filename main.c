bf9dc5a7aac9c20846637d7c2f86187112505cf0
yosefkahlon <yosefkahlon53@gmail.com>
2021-11-28 21:20:12 +0200

    c

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+##include <stdio.h>
 #include <stdio.h>
 #include "my_mat.h"
 
 int main()
 {
-    int mat[10][10];
-
+   int mat[10][10];
+   
     char user;
     scanf("%c", &user);
     while (user != 'D')
@@ -13,43 +13,44 @@ int main()
         if (user == 'A')
         {
             func1(mat);
+            
         }
         else if (user == 'B')
         {
-            int ans = func2(mat);
+            int ans=func2(mat);
             if (ans == 0)
-            {
-                printf("False");
-                printf("\n");
-            }
-            else
-            {
-                printf("True");
-                printf("\n");
-            }
+        {
+            printf("False");
+            printf("\n");
+        }
+    else
+        {
+            printf("True");
+            printf("\n");
+        }
+            
         }
         else if (user == 'C')
         {
-            int ans = func3(mat);
-            if (ans == 0)
-            {
-                printf("-1");
-                printf("\n");
-            }
-            else
-            {
-                printf("%d", ans);
-                printf("\n");
-            }
-        }
-        if (user == 'D')
-        {
-            return 0;
-        }
-        else
-        {
-            scanf("%c", &user);
-        }
+           int ans= func3(mat);
+           if (ans == 0)
+    {
+        printf("-1");
+        printf("\n");
     }
-    return 0;
+    else
+    {
+        printf("%d", ans);
+        printf("\n");
+    }
+         }
+        if (user=='D'){
+            return 0;
+        }else{
+            scanf("%c", &user); 
+        }
+ 
+        
+    }
+   return 0;
 }
